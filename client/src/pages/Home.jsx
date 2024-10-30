21ac1a84afd63427fb9c463f9efa27ada95a8f00
LEELA MANOHAR GUDIVADA <142682424+LEELAMANOHARGUDIVADA@users.noreply.github.com>
2024-10-30 21:46:29 +0530

    Update Home.jsx

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -8,11 +8,15 @@ import AuthContext from "@/context/AuthContext";
 import { useToast } from "@/hooks/use-toast";
 import img from '@/assets/threads.png'
 import MobileNav from "@/components/MobileNav";
+import { useTheme } from "@/components/theme-provider";
+import threads from "../assets/threads.png"
+import threads_light from "../assets/threads-light.png"
 
 const Home = () => {
   const { user, logout } = useContext(AuthContext);
 
   const {toast} = useToast();
+  const { theme } = useTheme();
 
   const handleLogOut = () => {
     logout();
@@ -27,7 +31,15 @@ const Home = () => {
       <div className='w-[75%] h-full flex items-center justify-center '>
         <Sidebar className='fixed h-full hidden md:block'  />
         <Link to={`/`} className="md:hidden absolute top-2 left-5 z-10">
-          <img src={img} alt="" className="w-10" />
+          { theme === "dark" ? <img
+          src={threads}
+          alt="threads"
+          className="w-10 hover:scale-105 transform duration-150"
+        /> : <img
+          src={threads_light}
+          alt="threads"
+          className="w-10 hover:scale-105 transform duration-150"
+        /> }
         </Link>
         <div className='w-2/4 h-full '>
             <Feed className="h-full" />
