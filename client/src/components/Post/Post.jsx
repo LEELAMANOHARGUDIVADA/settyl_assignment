ff9dd41ffd2b54302526ecaf5833e43dbfd4d057
LEELA MANOHAR GUDIVADA <142682424+LEELAMANOHARGUDIVADA@users.noreply.github.com>
2024-10-31 00:35:15 +0530

    Update Post.jsx

--- client/src/components/Post/Post.jsx
+++ client/src/components/Post/Post.jsx
@@ -1,17 +1,19 @@
-import React, { useEffect, useState } from "react";
+import React, { useEffect, useState, useContext } from "react";
 import axios from "axios";
 import { useNavigate, useParams } from "react-router-dom";
 import Comment from "../Comment";
 import { Loader } from "lucide-react";
 import { Button } from "../ui/button";
+import AuthContext from "@/context/AuthContext";
 
 const Post = () => {
   const [post, setPost] = useState();
   const [isLoading, setIsLoading] = useState(true);
-
+  const [profile, setProfile] = useState();
   const token = localStorage.getItem("token");
   const SERVERURL = import.meta.env.VITE_API_URL;
-
+  const { user } = useContext(AuthContext);
+ 
   const { id } = useParams();
   const fetchPost = async() => {
     try {
@@ -28,9 +30,25 @@ const Post = () => {
       setIsLoading(false);
     }
   }
+  const userId = user.id;
+   const fetchUserProfile = async () => {
+    try {
+      const response = await axios.get(`${SERVERURL}/api/user/getUserProfile/${id}`, {
+        headers: {
+          "Authorization": `Bearer ${token}`
+        }
+      });
+      setProfile(response.data.user);
+    } catch (error) {
+      console.log(error);
+    } finally {
+      setIsLoading(false);
+    }
+  };
 
   useEffect(() => {
     fetchPost();
+    fetchUserProfile();
   },[])
   return (
     <div>
@@ -43,7 +61,7 @@ const Post = () => {
         <h2 className="invisible md:visible text-center my-5 text-sm font-bold">{post.userId.name}'s Post</h2>
           <div className="w-[360px] sm:w-[420px]  md:w-[480px] lg:w-[580px] relative h-screen max-h-screen overflow-y-auto overflow-x-hidden  border rounded-t-3xl hide-scrollbar">
           <div className="sticky top-0 z-10 w-full ">
-            <Comment post={post}  />
+            <Comment post={post} profile={profile} />
           </div>
         </div>
       </div>
