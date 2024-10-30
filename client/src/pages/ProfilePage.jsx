ebd70c3e65bf217916a72346dd32ae8384a4ea81
LEELA MANOHAR GUDIVADA <142682424+LEELAMANOHARGUDIVADA@users.noreply.github.com>
2024-10-30 23:20:32 +0530

    Update ProfilePage.jsx

--- client/src/pages/ProfilePage.jsx
+++ client/src/pages/ProfilePage.jsx
@@ -6,9 +6,14 @@ import React, { useContext } from 'react'
 import { Link } from 'react-router-dom'
 import img from '@/assets/threads.png'
 import MobileNav from '@/components/MobileNav'
+import { useTheme } from "@/components/theme-provider";
+import threads from "../assets/threads.png"
+import threads_light from "../assets/threads-light.png"
+import { ModeToggle } from "@/components/mode-toggle";
 
 const ProfilePage = () => {
   const { user, logout } = useContext(AuthContext);
+  const { theme } = useTheme();
 
   const handleLogOut = () => {
     logout();
@@ -18,12 +23,25 @@ const ProfilePage = () => {
       <div className='w-[75%] h-full flex items-center justify-center '>
       <Sidebar className='fixed h-full hidden md:block'  />
         <Link to={`/`} className="md:hidden absolute top-2 left-5 z-10">
-          <img src={img} alt="" className="w-10" />
+          { theme === "dark" ? <img
+          src={threads}
+          alt="threads"
+          className="w-8 hover:scale-105 transform duration-150"
+        /> : <img
+          src={threads_light}
+          alt="threads"
+          className="w-14 hover:scale-105 transform duration-150"
+        /> }
         </Link>
         <div className='w-2/4 h-full'>
             <Profile className="h-screen" />
         </div>
-        {user ? <Link to={`/`} className='w-1/4 h-full'>
+        <div className="w-1/4 ">
+          <div className="absolute right-32 top-4">
+          <ModeToggle />
+          </div>
+          <div>
+          {user ? <Link to={`/`} className='w-1/4 h-full'>
           <Button variant='destructive' className="absolute top-4 right-5" onClick={handleLogOut}>
             Logout
           </Button>
@@ -32,6 +50,8 @@ const ProfilePage = () => {
             Login
           </Button>
         </Link>}
+          </div>
+        </div>
         </div>
 
         <MobileNav />
