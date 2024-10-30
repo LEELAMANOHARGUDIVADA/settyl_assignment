78c1bbcc90a37c47c940bb259cb71f4536866148
LEELA MANOHAR GUDIVADA <142682424+LEELAMANOHARGUDIVADA@users.noreply.github.com>
2024-10-30 23:15:57 +0530

    Update SearchPage.jsx

--- client/src/pages/SearchPage.jsx
+++ client/src/pages/SearchPage.jsx
@@ -6,14 +6,18 @@ import { Button } from "@/components/ui/button";
 import { Link } from "react-router-dom";
 import AuthContext from "@/context/AuthContext";
 import { useToast } from "@/hooks/use-toast";
-import img from '@/assets/threads.png'
 import MobileNav from "@/components/MobileNav";
 import Search from "@/components/Search/Search";
+import { ModeToggle } from "@/components/mode-toggle";
+import { useTheme } from "@/components/theme-provider";
+import threads from "../assets/threads.png"
+import threads_light from "../assets/threads-light.png"
 
 const SearchPage = () => {
   const { user, logout } = useContext(AuthContext);
 
   const {toast} = useToast();
+  const { theme } = useTheme();
 
   const handleLogOut = () => {
     logout();
@@ -28,12 +32,25 @@ const SearchPage = () => {
       <div className='w-[75%] h-full flex items-center justify-center '>
         <Sidebar className='fixed h-full hidden md:block'  />
         <Link to={`/`} className="md:hidden absolute top-2 left-5 z-10">
-          <img src={img} alt="" className="w-10" />
+          { theme === "dark" ? <img
+          src={threads}
+          alt="threads"
+          className="w-8 hover:scale-105 transform duration-150"
+        /> : <img
+          src={threads_light}
+          alt="threads"
+          className="w-14 hover:scale-105 transform duration-150"
+        /> }
         </Link>
         <div className='w-2/4 h-full'>
             <Search className="h-screen" />
         </div>
-        {user ? <Link to={`/`} className='w-1/4 h-full'>
+        <div className="w-1/4 ">
+          <div className="absolute right-32 top-4">
+          <ModeToggle />
+          </div>
+          <div>
+          {user ? <Link to={`/`} className='w-1/4 h-full'>
           <Button variant='destructive' className="absolute top-4 right-5" onClick={handleLogOut}>
             Logout
           </Button>
@@ -42,6 +59,8 @@ const SearchPage = () => {
             Login
           </Button>
         </Link>}
+          </div>
+        </div>
         </div>
         
         {/* Mobile Nav Bar  */}
